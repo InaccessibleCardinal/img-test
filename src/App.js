63dbd8c0479efab9ab0d8f688e0b7072f6810d6e
InaccessibleCardinal <kennethlandsbaum@hotmail.com>
2018-10-18 17:27:19 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {image: null, otherImage: null};
+  }
+
+  componentWillMount() {
+    let img1Url = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+    let img2Url = 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350';
+    
+    axios.get(img1Url, {responseType: 'arraybuffer'})
+      .then((response) => {
+
+        // console.log('headers: ', response.headers)
+        let u = btoa(
+          new Uint8Array(response.data)
+            .reduce((data, byte) => data + String.fromCharCode(byte), '')
+        );
+        
+        this.setState(
+          {image: `data:image/png;base64,${u}`}, 
+          () => console.log('state: ', this.state)
+        );
+
+      })
+      .catch((e) => console.log(e));
+
+      axios.get(img2Url, {responseType: 'blob'})
+        .then((response) => {
+
+          let _this = this;
+          let reader = new FileReader();
+          reader.readAsDataURL(response.data);
+
+          reader.onload = function() {
+            let result = reader.result;
+            _this.setState({otherImage: result});
+          }
+        })
+        .catch((e) => console.log(e));
+  }
   render() {
+    let {image, otherImage} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {image && <img src={image} width="200" alt="yourmom" />}
+        {otherImage && <img src={otherImage} width="200" alt="yourmom" />}
       </div>
     );
   }
